0a35abc9adb8b1221b1cc8b325a27e399eb54873
Diwakar Dubey <135966918+diwakar0712@users.noreply.github.com>
2024-06-18 21:50:47 +0530

    Add files via upload

---
+++ q6.js
@@ -0,0 +1,10 @@
+// Function to reverse a string
+function reverseString(input) {
+    return input.split('').reverse().join('');
+  }
+  
+  const originalString = "Hello, world!";
+  const reversedString = reverseString(originalString);
+  console.log(reversedString);
+  
+  
